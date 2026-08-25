b5d6400b04cf9b5931b209897cf4f83c2cb01048
Tyler101-coder <LifeStudio101@outlook.com>
2026-08-25 09:25:43 +0800

    Add BasicExample SwiftUI view with Bezel integration

--- Examples/Examples/BasicExample.swift
+++ Examples/Examples/BasicExample.swift
@@ -1,1 +1,19 @@
 
+import SwiftUI
+import Bezel
+
+struct BasicExample: View {
+    
+    var body: some View {
+        Bezel {
+            VStack {
+                Image(systemName: "iphone")
+                    .font(.largeTitle)
+
+                Text("Hello Bezel")
+                    .font(.title)
+            }
+            .padding()
+        }
+    }
+}
