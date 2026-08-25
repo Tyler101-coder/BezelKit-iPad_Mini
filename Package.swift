83ce6b2b00dae59c7f0f75a9350f2a5bf8f97441
Tyler101-coder <LifeStudio101@outlook.com>
2026-08-25 09:23:39 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,22 +1,29 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 
 import PackageDescription
 
 let package = Package(
     name: "Bezel",
+    
     platforms: [
         .iOS(.v17)
     ],
+    
     products: [
         .library(
             name: "Bezel",
             targets: ["Bezel"]
         )
     ],
+    
     targets: [
+        
+        // Main Package
         .target(
             name: "Bezel"
         ),
+        
+        // Unit Tests
         .testTarget(
             name: "BezelTests",
             dependencies: ["Bezel"]
